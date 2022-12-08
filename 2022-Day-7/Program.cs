1868e251fadd5185935b0d62047e438b2e15221d
Rubens <rubens.pirie@gmail.com>
2022-12-08 09:45:29 +0000

    I was too emarrased to post my day 8 as it was at 5 am -_-

--- 2022-Day-7/Program.cs
+++ 2022-Day-7/Program.cs
@@ -12,123 +12,14 @@ namespace _2022_Day_7
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            string raw = File.ReadAllText("../../input.txt");
-
-            List<(string, List<string>)> commandsList = new List<(string, List<string>)>();
-
-            string[] input = raw.Split('$');
-            
-            for (int i = 1; i < input.Length; i++)
-            {
-                List<string> contents = input[i].Split('\n').ToList();
-                string command = contents[0].Split('\r')[0];
-                contents.RemoveAt(0);
-
-                var temp = new List<string>();
-
-                foreach (var response in contents)
-                {
-                    if (response != "")
-                    {
-                        temp.Add(response.Split('\r')[0].Trim());
-                    }
-                }
-
-                commandsList.Add((command.Trim(), temp));
-            }
-
-            Dictionary<string, long> dirSizes = new Dictionary<string, long>();
-            Dictionary<string, List<string>> dirChildren = new Dictionary<string, List<string>>();
-
-            string currentLevel = "/";
-
-            for (int i = 0; i < commandsList.Count; i++)
-            {
-                if (commandsList[i].Item1 == "ls")
-                {
-                    int backI = 1;
-                    string prev = commandsList[i - backI].Item1;
-
-                    List<string> dirChain = new List<string>();
-
-                    while (prev.StartsWith("cd") && i - backI > 0)
-                    {
-                        dirChain.Add(commandsList[i - backI].Item1);
-                        backI++;
-                        prev = commandsList[i - backI].Item1;
-                    }
-
-                    string dir = ParseDirChain(currentLevel, dirChain);
-
-
-                    long tot = 0;
-                    long working = 0;
-
-                    List<string> children = new List<string>();
-
-                    for (int j = 0; j < commandsList[i].Item2.Count; j++)
-                    {
-                        if (long.TryParse(commandsList[i].Item2[j].Split(' ')[0], out working))
-                        {
-                            tot += working;
-                        }
-                        else
-                        {
-                            children.Add(commandsList[i].Item2[j].Split(' ')[1].Trim());
-                        }
-                    }
-
-                    dirSizes.Add(dir, tot);
-                    dirChildren.Add(dir, children);
-                    currentLevel = dir;
-                }
-            }
-
-
-            long tempTot;
-            foreach (var key in dirChildren.Keys)
-            {
-                dirSizes[key] = GetDirSize(dirSizes, dirChildren, key);
-            }
-
-            long total = 0;
-
-            foreach (var key in dirSizes.Keys)
-            {
-                if (dirSizes[key] <= 100000) total += dirSizes[key];
-            }
-
-            // Part 1
-            Console.WriteLine(total);
-
-            long someValue = 70000000 - dirSizes["/"];
-            long smallest = long.MaxValue;
-            foreach (var key in dirSizes.Keys)
-            {
-                if (someValue + dirSizes[key] > 30000000 && dirSizes[key] < smallest)
-                {
-                    smallest = dirSizes[key];
-                }
-            }
-            // Part 2
-            Console.WriteLine(smallest);
-            Console.ReadLine();
-        }
-
-        public static long GetDirSize(Dictionary<string, long> entries, Dictionary<string, List<string>> children,string toGetDir)
+        public static long GetDirSize(Dictionary<string, long> entries, Dictionary<string, List<string>> children, string toGetDir)
         {
             long newSize = entries[toGetDir];
 
             for (int i = 0; i < children[toGetDir].Count; i++)
             {
-                List<string> split =  toGetDir.Split('/').ToList();
-                for (int j = 0; j < split.Count; j++)
-                {
-                    if (split[j] == "") split.RemoveAt(j);
-                }
-
+                List<string> split = toGetDir.Split('/').ToList();
+                for (int j = 0; j < split.Count; j++) if (split[j] == "") split.RemoveAt(j);
                 newSize += GetDirSize(entries, children, (toGetDir == "/" ? "" : "/") + string.Join("/", split) + "/" + children[toGetDir][i] + "/");
             }
 
@@ -155,18 +46,84 @@ namespace _2022_Day_7
             }
 
             string result = "/";
+            for (int i = 0; i < stackOfThings.Count; i++) if (stackOfThings[i] != "") result += stackOfThings[i].Trim() + "/";
 
-            for (int i = 0; i < stackOfThings.Count; i++)
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string raw = File.ReadAllText("../../input.txt");
+
+            List<(string, List<string>)> commandsList = new List<(string, List<string>)>();
+
+            string[] input = raw.Split('$');
+            
+            for (int i = 1; i < input.Length; i++)
             {
-                if (stackOfThings[i] != "")
+                List<string> contents = input[i].Split('\n').ToList();
+                string command = contents[0].Split('\r')[0];
+                contents.RemoveAt(0);
+                var temp = new List<string>();
+
+                foreach (var response in contents) if (response != "") temp.Add(response.Split('\r')[0].Trim());
+                
+                commandsList.Add((command.Trim(), temp));
+            }
+
+            Dictionary<string, long> dirSizes = new Dictionary<string, long>();
+            Dictionary<string, List<string>> dirChildren = new Dictionary<string, List<string>>();
+
+            string currentLevel = "/";
+
+            for (int i = 0; i < commandsList.Count; i++)
+            {
+                if (commandsList[i].Item1 == "ls")
                 {
-                    result += stackOfThings[i].Trim();
-                    result += "/";
+                    int backI = 1;
+                    string prev = commandsList[i - backI].Item1;
+
+                    List<string> dirChain = new List<string>();
+
+                    while (prev.StartsWith("cd") && i - backI > 0)
+                    {
+                        dirChain.Add(commandsList[i - backI].Item1);
+                        backI++;
+                        prev = commandsList[i - backI].Item1;
+                    }
+
+                    string dir = ParseDirChain(currentLevel, dirChain);
+                    long tot = 0;
+                    long working;
+
+                    List<string> children = new List<string>();
+
+                    for (int j = 0; j < commandsList[i].Item2.Count; j++)
+                    {
+                        if (long.TryParse(commandsList[i].Item2[j].Split(' ')[0], out working)) tot += working;
+                        else children.Add(commandsList[i].Item2[j].Split(' ')[1].Trim());
+                    }
+
+                    dirSizes.Add(dir, tot);
+                    dirChildren.Add(dir, children);
+                    currentLevel = dir;
                 }
             }
 
+            foreach (var key in dirChildren.Keys) dirSizes[key] = GetDirSize(dirSizes, dirChildren, key);
+            long total = 0;
 
-            return result;
+            // Part 1
+            foreach (var key in dirSizes.Keys) if (dirSizes[key] <= 100000) total += dirSizes[key];
+            Console.WriteLine(total);
+            
+            // Part 2
+            long someValue = 70000000 - dirSizes["/"];
+            long smallest = long.MaxValue;
+            foreach (var key in dirSizes.Keys) if (someValue + dirSizes[key] > 30000000 && dirSizes[key] < smallest) smallest = dirSizes[key];
+            Console.WriteLine(smallest);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ 2022-Day-8/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+
+namespace _2022_Day_8
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("../../input.txt");
+
+            long partA = 0;
+            long partB = 0;
+
+            int[,] grid = new int[input[0].Length, input.Length];
+
+            for (int i = 0; i < input.Length; i++) for (int j = 0; j < input[i].Length; j++) grid[i, j] = int.Parse(input[i][j].ToString());
+            
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[0].Length; x++)
+                {
+                    if (IsVisible(x, y, grid))
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write(grid[y, x]);
+                        Console.ResetColor();
+                        partA++;
+                    }
+                    else
+                    {
+                        Console.Write(grid[y, x]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(partA);
+
+            for (int y = 1; y < input.Length - 1; y++)
+            {
+                for (int x = 1; x < input[0].Length - 1; x++)
+                {
+                    if ((GetScenicScore(x, y, grid) > partB))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(grid[y, x]);
+                        Console.ResetColor();
+                        partB = GetScenicScore(x, y, grid);
+                    }
+                    else
+                    {
+                        Console.Write(grid[y, x]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(partB);
+            Console.ReadLine();
+        }
+
+        public static long GetScenicScore(int x, int y, int[,] grid)
+        {
+            long value = 1;
+            long temp = 0;
+            for (int i = x - 1; i >= 0; i--)
+            {
+                temp++;
+                if (grid[y, i] >= grid[y, x]) break;
+            }
+
+            value *= temp;
+            temp = 0;
+            for (int i = x + 1; i < grid.GetLength(1); i++)
+            {
+                temp++;
+                if (grid[y, i] >= grid[y, x]) break;
+            }
+
+            value *= temp;
+            temp = 0;
+            for (int i = y - 1; i >= 0; i--)
+            {
+                temp++;
+                if (grid[i, x] >= grid[y, x]) break;
+            }
+
+            value *= temp;
+            temp = 0;
+            for (int i = y + 1; i < grid.GetLength(0); i++)
+            {
+                temp++;
+                if (grid[i, x] >= grid[y, x]) break;
+            }
+
+            value *= temp;
+            return value;
+        }
+
+        public static bool IsVisible(int x, int y, int[,] grid)
+        {
+            bool collided = false;
+            for (int i = 0; i < x; i++) if (grid[y, i] >= grid[y, x]) collided = true;
+            if (!collided) return true;
+
+            collided = false;
+            for (int i = x + 1; i < grid.GetLength(1); i++) if (grid[y, i] >= grid[y, x]) collided = true;
+            if (!collided) return true;
+
+            collided = false;
+            for (int i = 0; i < y; i++) if (grid[i, x] >= grid[y, x]) collided = true;
+            if (!collided) return true;
+
+            collided = false;
+            for (int i = y + 1; i < grid.GetLength(0); i++) if (grid[i, x] >= grid[y, x]) collided = true;
+            return !collided;
+        }
+    }
+}
