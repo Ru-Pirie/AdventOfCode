6baeddeb53867175da11142537437dd6adaf8319
Rubens <rubens.pirie@gmail.com>
2022-12-18 10:19:29 +0000

    Good bit of flood fill today

---
+++ 2022-Day-17/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _2022_Day_17
+{
+    internal class Program
+    {
+        public static string[][] blocks = {
+            new[] {
+                "@@@@"
+            },
+            new[] {
+                " @ ",
+                "@@@",
+                " @ "
+            },
+            new[] {
+                "@@@",
+                "  @",
+                "  @"
+            },
+            new[] {
+                "@",
+                "@",
+                "@",
+                "@"
+            },
+            new[] {
+                "@@",
+                "@@"
+            }
+        };
+
+        public static int block = 0;
+        public static string[] current = blocks[block];
+
+        public static Dictionary<(int X, int Y), int> map = new Dictionary<(int X, int Y), int>();
+
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("../../input.txt");
+
+            string tape = string.Join("", input);
+            int index = 0;
+            long repeats = 0;
+
+            int maxY = 0;
+            int dispMax = 0;
+
+            bool inPlay = false;
+
+            var (blockX, blockY) = (0, 0);
+
+            long nth = 0;
+
+            var revisit = new Dictionary<(int Tape, int Shape), (long Rocks, long Height)>();
+
+
+            long found1 = 0;
+            long found2 = 0;
+
+            while (found1 == 0 || found2 == 0)
+            {
+                if (!inPlay)
+                {
+                    blockX = 2;
+                    blockY = (map.Count > 1 ? maxY : -1) + 4;
+                    dispMax = blockY + blocks[block].Length;
+                    current = blocks[block];
+                    inPlay = true;
+                    nth++;
+                    if (nth == 2023)
+                    {
+                        found1 = maxY + 1;
+                    }
+                }
+
+                int next = blockX;
+                if (tape[index] == '<')
+                {
+                    next--;
+                }
+                else
+                {
+                    next++;
+                }
+                if (0 <= next && next + current[0].Length <= 7 && CheckCollision(next, blockY))
+                {
+                    blockX = next;
+                }
+                index = (index + 1) % tape.Length;
+                if (index == 0) repeats++;
+                next = blockY - 1;
+                if (0 <= next && CheckCollision(blockX, next))
+                {
+                    blockY = next;
+                }
+                else
+                {
+                    for (int y = 0; y < blocks[block].Length; y++)
+                    {
+                        for (int x = 0; x < blocks[block][y].Length; x++)
+                        {
+                            bool place = blocks[block][y][x] == '@';
+                            if (place) map[(blockX + x, blockY + y)] = block;
+                            maxY = Math.Max(maxY, blockY + y);
+                        }
+                    }
+                    block = (block + 1) % blocks.Length;
+                    inPlay = false;
+
+                    if (revisit.ContainsKey((index, block)) && found2 == 0)
+                    {
+                        var last = revisit[(index, block)];
+                        long cycle = nth - last.Rocks;
+                        long adds = maxY + 1 - last.Height;
+                        long remaining = 1000000000000 - nth - 1;
+                        long combo = (remaining / (cycle) + 1);
+                        if (nth + combo * cycle == 1000000000000)
+                            found2 = maxY + 1 + combo * adds;
+                    }
+                    else
+                    {
+                        revisit[(index, block)] = (nth, maxY + 1);
+                    }
+                }
+            }
+            Console.WriteLine($"Part 1: {found1}");
+            Console.WriteLine($"Part 2: {found2}");
+            Console.ReadKey();
+        }
+
+        public static bool CheckCollision(int bx, int by)
+        {
+            bool ok = true;
+            for (int y = by; y < by + current.Length; y++)
+            {
+                for (int x = bx; x < bx + current[y - by].Length; x++)
+                {
+                    int read = map.ContainsKey((x, y)) ? map[(x, y)] : -1;
+                    if (by <= y && y <= by + current.Length - 1 && bx <= x && x <= bx + current[y - by].Length - 1)
+                    {
+                        if (current[y - by][x - bx] == '@' && read != -1)
+                        {
+                            ok = false;
+                        }
+                    }
+                }
+            }
+            return ok;
+        }
+    }
+}
---
+++ 2022-Day-18/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2022_Day_18
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("../../input.txt");
+
+            int min = 100;
+            int max = 0;
+
+            List<List<int>> cubes = new List<List<int>>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] bits = input[i].Split(',');
+                int[] numBits = new int[3];
+
+                for (int j = 0; j < bits.Length; j++)
+                {
+                    numBits[j] = int.Parse(bits[j]);
+
+                    max = Math.Max(max, numBits[j]);
+                    min = Math.Min(min, numBits[j]);
+                }
+
+                cubes.Add(numBits.ToList());
+            }
+
+            HashSet<int> sides = new HashSet<int>();
+            foreach (List<int> cube in cubes)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    int code = 0;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        code = code * 100 + cube[j] * 2;
+                        if (j == i / 2)
+                        {
+                            code += (i % 2 == 0 ? 1 : -1);
+                        }
+                    }
+                    sides.Add(code);
+                }
+            }
+
+            int connected = cubes.Count * 6 - sides.Count;
+            int shown = sides.Count - connected;
+            Console.WriteLine($"Part 1: {shown}");
+
+            List<(int, int, int)> trace = new List<(int, int, int)>();
+
+            HashSet<int> blocks = new HashSet<int>();
+            foreach (List<int> cube in cubes)
+            {
+                blocks.Add(cube[0] * 10000 + cube[1] * 100 + cube[2]);
+            }
+
+            max++;
+            min--;
+
+            List<(int, int, int)> steam = new List<(int, int, int)> {
+                (min, min, min),
+                (min, min, max),
+                (min, max, min),
+                (min, max, max),
+                (max, min, min),
+                (max, min, max),
+                (max, max, min),
+                (max, max, max)
+            };
+
+            List<(int, int, int)> dirs = new List<(int, int, int)>
+            {
+                (-1, 0, 0),
+                ( 1, 0, 0),
+                ( 0,-1, 0),
+                ( 0, 1, 0),
+                ( 0, 0,-1),
+                ( 0, 0, 1)
+            };
+
+            HashSet<int> visited = new HashSet<int>
+            {
+                10000 * min + 100 * min + min
+            };
+
+            HashSet<int> sides2 = new HashSet<int>();
+
+            while (steam.Count > 0)
+            {
+                trace.AddRange(steam);
+                var more = new List<(int, int, int)>(steam.Count);
+                foreach ((int x, int y, int z) in steam)
+                {
+                    foreach ((int dx, int dy, int dz) in dirs)
+                    {
+                        if (x + dx < min || y + dy < min || z + dz < min) continue;
+                        if (x + dx > max || y + dy > max || z + dz > max) continue;
+                        int code = (x + dx) * 10000 + (y + dy) * 100 + (z + dz);
+                        if (visited.Contains(code)) continue;
+                        if (blocks.Contains(code))
+                        {
+                            sides2.Add((x * 2 + dx) * 10000 + (y * 2 + dy) * 100 + (z * 2 + dz));
+                            continue;
+                        }
+                        visited.Add(code);
+                        more.Add((x + dx, y + dy, z + dz));
+                    }
+                }
+                steam = more;
+            }
+
+
+            Console.WriteLine($"Part 2: {sides2.Count}");
+            Console.ReadLine();
+        }
+    }
+}
